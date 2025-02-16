916cab6502acc3530193236be82efb58b17df4c5
Milan Singla <168563817+Milansingla@users.noreply.github.com>
2025-02-16 15:37:50 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,7 +5,11 @@ int main()
     scanf("%d %d %c",&a,&b,&c);
     if(c=='+'){printf("%d",a+b);}
     else if(c=='-'){printf("%d",a-b);}
-   else  if(c=='/'){printf("%d",a/b);}
+   else  if(c=='/'){if (b == 0) {
+            printf("Error: Division by zero is not allowed\n");
+        } else {
+            printf("Result: %.2f\n", (float)a / b);
+        }}
     else if(c=='*'){printf("%d",a*b);}
     else{printf("Invalid Operator");}
     return 0;
